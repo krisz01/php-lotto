01c09d21ef3accb0cdf60302180f076dfcdd65d6
diak <diak@10.9.8.10>
2019-02-19 08:00:20 +0100

    lottoszelvény fgv

--- fgv.php
+++ fgv.php
@@ -15,5 +15,35 @@
     return $tomb;
     //var_dump($tomb);
     }
+    
+    function lottoszelveny($sorsolt)
+     {
+        $szamokszama=90;
+        $oszlop=15;
+        echo '<table border="1">';
+	for($i=1; $i<=$szamokszama; $i++)
+	{
+		if($i%$oszlop==1)
+		{
+			echo "<tr>";
+		}
+		if(isset($sorsolt[$i]))
+		{
+			$hatterszin="#FF0000";
+		}
+		else
+		{
+			$hatterszin="FFFFFF";
+		}
+		echo "<td bgcolor=\"$hatterszin\">";
+			echo $i;
+		echo "</td>";
+		if($i%$oszlop==0)
+		{
+			echo "</tr>\n";
+		}
+	}
+	echo "</table>";
+     }
 
 ?>
