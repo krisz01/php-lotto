52f4532f6c3970d37a60ea2e2b3ef833dabaed7c
diak <diak@10.9.8.10>
2019-02-19 07:59:51 +0100

    függvény használat, függvények áthelyezése fv.php-be

--- index.php
+++ index.php
@@ -10,33 +10,8 @@
             include("fgv.php");
             $sorsolt=sorsolas();
             var_dump($sorsolt);
-            
-        $szamokszama=90;
-        $oszlop=15;
-        echo '<table border="1">';
-	for($i=1; $i<=$szamokszama; $i++)
-	{
-		if($i%$oszlop==1)
-		{
-			echo "<tr>";
-		}
-		if(isset($sorsolt[$i]))
-		{
-			$hatterszin="#FF0000";
-		}
-		else
-		{
-			$hatterszin="FFFFFF";
-		}
-		echo "<td bgcolor=\"$hatterszin\">";
-			echo $i;
-		echo "</td>";
-		if($i%$oszlop==0)
-		{
-			echo "</tr>\n";
-		}
-	}
-	echo "</table>";
+     
+     lottoszelveny($sorsolt);
         ?>
     </body>
 </html>
